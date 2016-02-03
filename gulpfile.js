70416d6764ce38dc8eb797f59d8f0977260c053a
Измайлов Никита < >
2016-02-03 18:45:36 +0300

    now for sure almost done less

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'), // Сообственно Gulp JS
     less = require('gulp-less');
 
 gulp.task('less', function() {
-    return gulp.src('less/main1.less')
+    return gulp.src('less/*.less')
         .pipe(less())
         .pipe(gulp.dest('css'))
 });
